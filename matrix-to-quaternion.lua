89fc5334d66b4fe6dcece4b4d3766a297349fcb8
EmilyBendsSpace <86985485+EmilyBendsSpace@users.noreply.github.com>
2023-02-17 12:53:51 -0800

    fix CFrame component variable names to col_row format

--- matrix-to-quaternion.lua
+++ matrix-to-quaternion.lua
@@ -3,34 +3,34 @@
 		by Sheppard's voting scheme method
 ]]--
 function Utils.MatrixToQuaternionSheppard( cf )
-	local tx, ty, tz, xx, xy, xz, yx, yy, yz, zx, zy, zz = cf:components()
+	local tx, ty, tz, xx, yx, zx, xy, yy, zy, xz, yz, zz = cf:components()
 	local tr = xx + yy + zz
 	local qx,qy,qz,qw = 0,0,0,1
 	local qv = nil
 	if tr > 0 then
 		local k = 0.5 / math.sqrt(1+tr)
-		qx = k * (zy-yz)
-		qy = k * (xz-zx)
-		qz = k * (yx-xy)
+		qx = k * (yz-zy)
+		qy = k * (zx-xz)
+		qz = k * (xy-yx)
 		qw = 0.25 / k
 	elseif (xx > yy) and (xx > zz) then
 		local k = 0.5 / math.sqrt(1+xx-yy-zz)
 		qx = 0.25/k
-		qy = k * (xy+yx)
-		qz = k * (xz+zx)
-		qw = k * (zy-yz)
+		qy = k * (yx+xy)
+		qz = k * (zx+xz)
+		qw = k * (yz-zy)
 	elseif (yy > zz) then
 		local k = 0.5 / math.sqrt(1+yy-xx-zz)
-		qx = k * (xy+yx)
+		qx = k * (yx+xy)
 		qy = 0.25 / k
-		qz = k * (zy+yz)
-		qw = k * (xz-zx)
+		qz = k * (yz+zy)
+		qw = k * (zx-xz)
 	else
 		local k = 0.5 / math.sqrt(1+zz-xx-yy)
-		qx = k * (xz+zx)
-		qy = k * (yz+zy)
+		qx = k * (zx+xz)
+		qy = k * (zy+yz)
 		qz = 0.25 / k
-		qw = k * (yx-xy)
+		qw = k * (xy-yx)
 	end
 	return {w=qw,x=qx,y=qy,z=qz}
 end
@@ -42,20 +42,20 @@ end
 		Slower but more numerically accurate than Shepard's method
 ]]--
 function Utils.MatrixToQuaternionSarabandi( cf )
-	local _,_,_,xx,xy,xz,yx,yy,yz,zx,zy,zz = cf:GetComponents()
-	local A = (zy-yz)*(zy-yz)
-	local B = (xz-zx)*(xz-zx)
-	local C = (yx-xy)*(yx-xy)
-	local D = (zy+yz)*(zy+yz)
-	local E = (zx+xz)*(zx+xz)
-	local F = (yx+xy)*(yx+xy)
+	local _,_,_,xx,yx,zx,xy,yy,zy,xz,yz,zz = cf:GetComponents()
+	local A = (yz-zy)*(yz-zy)
+	local B = (zx-xz)*(zx-xz)
+	local C = (xy-yx)*(xy-yx)
+	local D = (yz+zy)*(yz+zy)
+	local E = (xz+zx)*(xz+zx)
+	local F = (xy+yx)*(xy+yx)
 	local G = (1+xx+yy+zz)*(1+xx+yy+zz) 
 	local H = (1+xx-yy-zz)*(1+xx-yy-zz)
 	local I = (1+yy-xx-zz)*(1+yy-xx-zz)
 	local J = (1+zz-xx-yy)*(1+zz-xx-yy)
 	local qw = 0.25 * math.sqrt(A+B+C+G)
-	local qx = 0.25 * math.sqrt(A+E+F+H) * (if zy-yz>0 then 1 else -1)
-	local qy = 0.25 * math.sqrt(B+D+F+I) * (if xz-zx>0 then 1 else -1)
-	local qz = 0.25 * math.sqrt(C+D+E+J) * (if yx-xy>0 then 1 else -1)
+	local qx = 0.25 * math.sqrt(A+E+F+H) * (if yz-zy>0 then 1 else -1)
+	local qy = 0.25 * math.sqrt(B+D+F+I) * (if zx-xz>0 then 1 else -1)
+	local qz = 0.25 * math.sqrt(C+D+E+J) * (if xy-yx>0 then 1 else -1)
 	return {w=qw,x=qx,y=qy,z=qz}
 end
